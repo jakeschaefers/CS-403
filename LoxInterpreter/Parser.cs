e44f2ff8eee92e409f4f4e21e0d96e51485cc8e5
jakeschaefers <69828057+jakeschaefers@users.noreply.github.com>
2023-12-06 11:33:28 -0600

    Part 3 / Chap 6

---
+++ LoxInterpreter/Parser.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.Generic;
+
+namespace LoxInterpreter
+{
+    class Parser
+    {
+        private readonly List<Token> tokens;
+        private int current = 0;
+
+        public Parser(List<Token> tokens)
+        {
+            this.tokens = tokens;
+        }
+
+        public Expr Parse()
+        {
+            try
+            {
+                return Expression();
+            }
+            catch (ParseErrorException)
+            {
+                return null;
+            }
+        }
+
+        private Expr Expression()
+        {
+            return Equality();
+        }
+
+        private Expr Equality()
+        {
+            Expr expr = Comparison();
+
+            while (Match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL))
+            {
+                Token operatorToken = Previous();
+                Expr right = Comparison();
+                expr = new Expr.Binary(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Comparison()
+        {
+            Expr expr = Term();
+
+            while (Match(TokenType.GREATER, TokenType.GREATER_EQUAL,
+                         TokenType.LESS, TokenType.LESS_EQUAL))
+            {
+                Token operatorToken = Previous();
+                Expr right = Term();
+                expr = new Expr.Binary(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Term()
+        {
+            Expr expr = Factor();
+
+            while (Match(TokenType.MINUS, TokenType.PLUS))
+            {
+                Token operatorToken = Previous();
+                Expr right = Factor();
+                expr = new Expr.Binary(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Factor()
+        {
+            Expr expr = Unary();
+
+            while (Match(TokenType.SLASH, TokenType.STAR))
+            {
+                Token operatorToken = Previous();
+                Expr right = Unary();
+                expr = new Expr.Binary(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+
+        private Expr Unary()
+        {
+            if (Match(TokenType.BANG, TokenType.MINUS))
+            {
+                Token operatorToken = Previous();
+                Expr right = Unary();
+                return new Expr.Unary(operatorToken, right);
+            }
+
+            return Primary();
+        }
+
+        private Expr Primary()
+        {
+            if (Match(TokenType.FALSE)) return new Expr.Literal(false);
+            if (Match(TokenType.TRUE)) return new Expr.Literal(true);
+            if (Match(TokenType.NIL)) return new Expr.Literal(null);
+
+            if (Match(TokenType.NUMBER, TokenType.STRING))
+            {
+                return new Expr.Literal(Previous().Literal);
+            }
+
+            if (Match(TokenType.LEFT_PAREN))
+            {
+                Expr expr = Expression();
+                Consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+                return new Expr.Grouping(expr);
+            }
+
+            throw new ParseErrorException("Expect expression.");
+        }
+
+        private bool Match(params TokenType[] types)
+        {
+            foreach (var type in types)
+            {
+                if (Check(type))
+                {
+                    Advance();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool Check(TokenType type)
+        {
+            if (IsAtEnd()) return false;
+            return Peek().Type == type;
+        }
+
+        private Token Advance()
+        {
+            if (!IsAtEnd()) current++;
+            return Previous();
+        }
+
+        private bool IsAtEnd()
+        {
+            return Peek().Type == TokenType.EOF;
+        }
+
+        private Token Peek()
+        {
+            return tokens[current];
+        }
+
+        private Token Previous()
+        {
+            return tokens[current - 1];
+        }
+
+        private void Consume(TokenType type, string message)
+        {
+            if (Check(type))
+                Advance();
+            else
+                throw new ParseErrorException(message);
+        }
+
+        private class ParseErrorException : Exception
+        {
+            public ParseErrorException(string message) : base(message) { }
+        }
+    }
+}
