89019b8e2d448ee5ce44360f2119849aad6d6e34
jakeschaefers <69828057+jakeschaefers@users.noreply.github.com>
2023-12-08 03:37:25 -0600

    Parts 4-6

--- Bytecode/chunk.h
+++ Bytecode/chunk.h
@@ -7,10 +7,17 @@
 typedef enum
 {
     OP_CONSTANT,
+    OP_NIL,
+    OP_TRUE,
+    OP_FALSE,
+    OP_EQUAL,
+    OP_GREATER,
+    OP_LESS,
     OP_ADD,
     OP_SUBTRACT,
     OP_MULTIPLY,
     OP_DIVIDE,
+    OP_NOT,
     OP_NEGATE,
     OP_RETURN,
 } OpCode;
@@ -28,7 +35,6 @@ typedef struct
 void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
 void writeChunk(Chunk *chunk, uint8_t byte, int line);
-
 int addConstant(Chunk *chunk, Value value);
 
 #endif
